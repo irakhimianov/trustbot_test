080e077f55b6a6c4f18d2e74e062521d43b379e5
rkjar <rkjar@yandex.ru>
2022-10-25 23:59:39 +0500

    cancel keyboard, handler added

--- bot/keyboards/inline/cancel.py
+++ bot/keyboards/inline/cancel.py
@@ -8,3 +8,5 @@ cancel_kbd.add(cancel_button)
 dialog_cancel_kbd = InlineKeyboardMarkup(row_width=1)
 dialog_cancel_button = InlineKeyboardButton(text='❌📞 Завершить диалог', callback_data='cancel_dialog')
 dialog_cancel_kbd.add(dialog_cancel_button)
+
+skip_kbd = InlineKeyboardMarkup(row_width=1)
