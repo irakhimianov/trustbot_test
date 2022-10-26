37b98348261fba32dfbe7d13c1038636148ed93b
rkjar <rkjar@yandex.ru>
2022-10-26 09:20:48 +0500

    config example. rename to config later

--- bot/data/confg_example.py
+++ bot/data/confg_example.py
@@ -6,4 +6,6 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('BOT_TOKEN')
 ADMIN = os.getenv('ADMIN_ID')
-ADMINS = []
+ADMINS = [ADMIN, ]
+SUGGESTION_CHAT = os.getenv('SUGGESTION_CHAT')
+REQUEST_CHAT = os.getenv('REQUEST_CHAT')
